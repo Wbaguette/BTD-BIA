dad97bab19159886b72c04f14b6a3758337c3984
Arcey <44277279+Arcey-bot@users.noreply.github.com>
2024-02-24 17:41:46 -0500

    only exits on space press now

--- src/stage.asm
+++ src/stage.asm
@@ -1,5 +1,5 @@
 .model small
-.stack
+.stack 200h ; 512 bytes
 ; DOCS - https://stanislavs.org/helppc/idx_interrupt.html 
 .data
 VGA_VIDEO_SEGMENT       equ     0a000h  ;VGA display memory segment
@@ -11,13 +11,11 @@ main PROC
     mov al, 13h ; video mode (320x200, 256 color)
             ; each pixel is one byte in this mode
     int 10h ; trigger change to video mode
+
     mov ax, VGA_VIDEO_SEGMENT ; move vram to data segment
     mov es, ax ; es points to VGA memory (es can only be accessed via register)
   
-    ; mov ah, 06h ; 06h triggers scroll down
-    ; mov al, 0 ; no idea what this does but it breaks without it
-
-    mov al, 100
+    mov al, 100 ; color
     mov bx, 0 ;pixel offset 0 (none)
     mov cx, 64000 ;pixel count
 
@@ -26,18 +24,17 @@ main PROC
         inc bx
         dec al
         loop drawloop
+    
+    awaitkey: ; terminates program on key press
+        mov ah, 10h
+        int 16h
+        cmp al, 32 ; space key is pressed, terminate
+        je exit
+        jmp awaitkey ; space was not pressed
 
-    ; drawloop:
-    ;     mov cl, 5 ; start x position
-    ;     mov dl, 35 ; end x position
-    ;     ; mov ch, 0 ; start y position
-    ;     ; mov dh, 1 ; end y position
-    ;     mov bh, 44h ; color
-    ;     inc ch ; draw next line
-    ;     inc dh 
-    ;     dec bh ; change color
-    ;     cmp ch, 25 ; 25 lines drawn
-    ;     jnz drawloop
-
+    exit: ; terminates program
+        xor ax, ax ; zero out
+        mov ah, 4ch ; terminate process code
+        int 21h ; trigger exit
 main ENDP
 END main 
