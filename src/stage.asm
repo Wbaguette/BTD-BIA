5d5193f2f9b54c8c0cebc29acf3b31a73cbc4282
Arcey <44277279+Arcey-bot@users.noreply.github.com>
2024-02-24 04:03:00 -0500

    more drawing

--- src/stage.asm
+++ src/stage.asm
@@ -9,37 +9,35 @@ main PROC
     ;set video mode 
     mov ah, 00h
     mov al, 13h ; video mode (320x200, 256 color)
-
+            ; each pixel is one byte in this mode
     int 10h ; trigger change to video mode
-    mov ax, VGA_VIDEO_SEGMENT
-    mov es, ax ; es points to VGA memory
-    mov ah, 06h ; 06h triggers scroll down
-    mov al, 20 ; number of lines to scroll
-    jmp draw
-    
-    EndFlipPage: ; Exit if a key's been hit.
-        mov     ah,1
-        int     16h
-        jnz     Done
-        jmp     draw
-    
-    Done: ; Finished, clear key, reset screen mode and exit.
-        mov     ah,0    ;clear key
-        int     16h
-        mov     ax,3    ;reset to text mode
-        int     10h
-        mov     ah,4ch  ;exit to DOS
-        int     21h
-    draw:
-        mov cl, 0 ; start x position
-        mov dl, 25 ; end x position
-        ;ch specif2es the upper y axis coordinate
-        mov ch, 2 ; start y position
-        mov dh, 15 ; end y position
-        ;bh specifies the background/foreground color and cursor handling 
-        ;bh takes an 8 bit binary number
-        ;the "b" at the end specifies binary
-        mov bh , 01110000b
+    mov ax, VGA_VIDEO_SEGMENT ; move vram to data segment
+    mov es, ax ; es points to VGA memory (es can only be accessed via register)
+  
+    ; mov ah, 06h ; 06h triggers scroll down
+    ; mov al, 0 ; no idea what this does but it breaks without it
+
+    mov al, 100
+    mov bx, 0 ;pixel offset 0 (none)
+    mov cx, 64000 ;pixel count
+
+    drawloop:
+        mov es:[bx], ax
+        inc bx
+        dec al
+        loop drawloop
+
+    ; drawloop:
+    ;     mov cl, 5 ; start x position
+    ;     mov dl, 35 ; end x position
+    ;     ; mov ch, 0 ; start y position
+    ;     ; mov dh, 1 ; end y position
+    ;     mov bh, 44h ; color
+    ;     inc ch ; draw next line
+    ;     inc dh 
+    ;     dec bh ; change color
+    ;     cmp ch, 25 ; 25 lines drawn
+    ;     jnz drawloop
 
 main ENDP
 END main 
