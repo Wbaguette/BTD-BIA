c4f6f7a6e2015c39ca96ab498b960fcfe984cac3
Arcey <44277279+Arcey-bot@users.noreply.github.com>
2024-02-24 18:10:23 -0500

    change screen color on click ESC, exit on click SPACE

--- src/stage.asm
+++ src/stage.asm
@@ -2,8 +2,8 @@
 .stack 200h ; 512 bytes
 ; DOCS - https://stanislavs.org/helppc/idx_interrupt.html 
 .data
-VGA_VIDEO_SEGMENT       equ     0a000h  ;VGA display memory segment
-
+    VGA_VIDEO_SEGMENT       equ     0a000h  ;VGA display memory segment 
+    color db 0
 .code
 main PROC
     ;set video mode 
@@ -15,22 +15,32 @@ main PROC
     mov ax, VGA_VIDEO_SEGMENT ; move vram to data segment
     mov es, ax ; es points to VGA memory (es can only be accessed via register)
   
-    mov al, 100 ; color
+    mov al, color ; color
     mov bx, 0 ;pixel offset 0 (none)
     mov cx, 64000 ;pixel count
 
     drawloop:
-        mov es:[bx], ax
-        inc bx
-        dec al
-        loop drawloop
-    
+        mov es:[bx], ax ; draw pixel
+        inc bx ; go to next pixel 
+        loop drawloop  
+
     awaitkey: ; terminates program on key press
         mov ah, 10h
         int 16h
         cmp al, 32 ; space key is pressed, terminate
         je exit
+        cmp al, 27 ; escape key is pressed, redraw
+        je redraw
         jmp awaitkey ; space was not pressed
+
+    redraw:
+        mov cx, 64000
+        xor bx, bx
+        xor ax, ax ; clear
+        mov al, color ; get previous draw color
+        add al, 5 ; change color
+        mov color, al ; save 
+        jmp drawloop 
 
     exit: ; terminates program
         xor ax, ax ; zero out
