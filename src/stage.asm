ca60e88d4a2b9d09abe6fc4fd8a4e138c57a3f4a
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2024-02-29 22:52:59 -0500

    ¯\_(ツ)_/¯

--- src/stage.asm
+++ src/stage.asm
@@ -8,6 +8,9 @@ include shapes.inc
     X_PIXELS equ 320
     Y_PIXELS equ 200
     CHUNK_SIZE equ 8
+    X_CHUNKS equ 40
+    Y_CHUNKS equ 25
+    TOTAL_CHUNKS equ 1000
     VRAM  equ 0a000h  ; VGA VRAM begins at this location
     PURPLE equ 22h
     TEAL equ 33h
@@ -16,8 +19,6 @@ include shapes.inc
     LIGHT_GRAY equ 66h
     GREEN equ 77h
     RED equ 88h
-
-    color db 0
 .code
 main PROC
     ;set video mode 
@@ -30,49 +31,49 @@ main PROC
 
     mov ax, VRAM ; move vram to data segment
     mov es, ax ; es points to VGA memory (es can only be accessed via register)
-    mov al, TEAL ; color
     mov bx, 0 ;pixel offset 0 (none)
-    mov cx, X_PIXELS
-    mov cx, 64000 ;pixel count
+    mov cx, TOTAL_CHUNKS
 
     drawloop:
+        ; ZERO ZERO FUCKING ZERO ZERO ZERO ZERO
         xor ax, ax
+        xor bx, bx
         xor dx, dx
-        mov dl, TEAL
-        call draw_chunk
-        xor ax, ax
-        xor dx, dx 
-        mov dl, YELLOW
-        mov ah, 1
-        mov al, 1
-        call draw_chunk
-        xor ax, ax
-        xor dx, dx 
-        mov dl, PURPLE
-        mov ah, 2
-        mov al, 2
-        call draw_chunk
-        ; loop drawloop
 
+        mov ax, cx ; setup for div
+        mov bx, Y_CHUNKS ; THIS BREAKS WITH DX IDK WHY
+        div bx  ; X chunk
+        mov ah, al ; conform to parameter order
+        mov al, dl ; y chunk
+
+        ; only draw if a perimeter piece
+        cmp ah, 39
+        je dothing
+        cmp al, 24
+        je dothing
+        cmp al, 0
+        je dothing
+        loop drawloop
+    
     awaitkey: ; terminates program on key press
         mov ah, 10h
         int 16h
         cmp al, 32 ; space key is pressed, terminate
         je exit
-        cmp al, 27 ; escape key is pressed, redraw
-        je redraw
         jmp awaitkey ; space was not pressed
-
-    redraw:
-        mov cx, 64000
-        xor bx, bx ; clear offset
-        xor ax, ax ; clear (usually keydata)
-        mov al, color ; get last color we drew
-        add al, 5 ; change color
-        mov color, al ; save as last color drawn
-        jmp drawloop  ; redraw
+    
+    dothing:
+        push cx
+        mov dl, TEAL ; set color
+        call draw_chunk
+        pop cx
+        dec cx
+        jmp drawloop
 
     exit: ; terminates program
+        xor ax, ax ; zero out
+        mov     ax,3    ;reset to text mode
+        int     10h
         xor ax, ax ; zero out
         mov ah, 4ch ; terminate process code
         int 21h ; trigger exit
