a47689d37f81272b81be658c4c90ab101eed446b
jeanpierre <jeanpierre@derbes.net>
2024-03-04 22:14:51 -0500

    kinda maybe maybe have something that kinda maybe works

--- resources.md
+++ resources.md
@@ -1,3 +1,6 @@
 Instructions - https://teaching.idallen.com/dat2343/09f/notes/25BasicAssembler.htm
 Same guy's class - https://teaching.idallen.com/dat2343/09f/notes/
 Other Instructions - http://www.husseinsspace.com/teaching/udw/1996/asmnotes/chaptwo.htm
+
+Sprite G.O.A.T. - https://www.youtube.com/watch?v=qS9XhNwZHe4     
+Sprite G.O.A.T. - https://www.chibialiens.com/8086/simplesamples.php#LessonS1
--- src/sprite.asm
+++ src/sprite.asm
@@ -4,13 +4,25 @@
 include macros.inc
 
 .data
-   VRAM equ 0A000h
-   SHIFT equ 1
+  
+sprite DB 00h, 00h, 0Eh, 0Eh, 0Eh, 0Eh, 00h, 00h,
+          00h, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 00h,
+          0Eh, 0Eh, 03h, 0Eh, 0Eh, 03h, 0Eh, 0Eh,
+          0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh,
+          0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh,
+          0Eh, 0Eh, 02h, 0Eh, 0Eh, 02h, 0Eh, 0Eh,
+          00h, 0Eh, 0Eh, 02h, 02h, 0Eh, 0Eh, 00h,
+          00h, 00h, 0Eh, 0Eh, 0Eh, 0Eh, 00h, 00h,
 .code
 main PROC
    setup
+   mov ax, @data
+   mov ds, ax
 
-
+   ; Let's draw the sprite at chunk DH, DL
+   mov dh, 3
+   mov dl, 3
+   call ShowSprite
    
 
 
@@ -18,5 +30,48 @@ main PROC
    
    exit
 main ENDP
+
+; XORs the sprite map, meaning that we can easily restore the background, by XORing again
+; Careful: Showing the sprite (calling this function) to the same position twice will REMOVE it
+; 
+ShowSprite PROC
+   ; DX gets modified so lets push this, chunks are 8
+   push dx
+   mov ax, 8
+   mul dh
+   ; Use DI (Destination Index) as the start of where we need to draw
+   mov di, ax
+
+   mov ax, 8*320
+   xor bx, bx
+   add bl, dl
+   mul bx
+   
+   add di, ax
+   pop dx
+
+   mov si, OFFSET sprite
+   ; Use cx for counting pixels drawn in chunk,  cl for height and ch for width
+   mov cl, 8
+   draw_y:
+      push di
+      mov ch, 8
+   draw_x:
+      mov al, ds:[si]
+      xor al, es:[di]
+      mov es:[di], al
+      inc si
+      inc di
+      dec ch
+      jnz draw_x
+      pop di
+      add di, 320
+      inc bl
+      dec cl
+      jnz draw_y
+   
+   ret
+ShowSprite ENDP
+
 
 end main
