7b6868dd2c2f375ee1d663c24e858aac44dd5f66
jeanpierre <jeanpierre@derbes.net>
2024-03-10 14:40:09 -0400

    sprite mapping working

--- src/sprite/example.asm
+++ src/sprite/example.asm
@@ -12,11 +12,6 @@ include module.inc
 main PROC
 	setup
 
-	; mov dh, 15 ; WIDTH, Draw sprite at X chunk
-	; mov dl, 15 ; HEIGHT, Draw sprite at Y chunk
-	; mov cx, SMILE ; Sprite we want to draw, this maps to an index in the include file 
-	; call ShowSprite
-
 	mov dh, 39 ; WIDTH, Draw sprite at X chunk
 	mov dl, 24 ; HEIGHT, Draw sprite at Y chunk
 	mov cx, SMILE2 ; Sprite we want to draw, this maps to an index in the include file 
@@ -30,6 +25,11 @@ main PROC
 	mov dh, 15 ; WIDTH, Draw sprite at X chunk
 	mov dl, 15 ; HEIGHT, Draw sprite at Y chunk
 	mov cx, SMILE ; Sprite we want to draw, this maps to an index in the include file 
+	call ShowSprite
+
+	mov dh, 34 ; WIDTH, Draw sprite at X chunk
+	mov dl, 0 ; HEIGHT, Draw sprite at Y chunk
+	mov cx, BUG ; Sprite we want to draw, this maps to an index in the include file 
 	call ShowSprite
 
 	exit
