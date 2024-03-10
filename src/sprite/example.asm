352a913add9bf1e14994c364c8f7f77dda97246f
jeanpierre <jeanpierre@derbes.net>
2024-03-10 11:41:12 -0400

    this is beautiful (is that how u spell it?)

--- src/sprite/example.asm
+++ src/sprite/example.asm
@@ -17,12 +17,17 @@ main PROC
 	; mov cx, SMILE ; Sprite we want to draw, this maps to an index in the include file 
 	; call ShowSprite
 
-	mov dh, 15 ; WIDTH, Draw sprite at X chunk
-	mov dl, 15 ; HEIGHT, Draw sprite at Y chunk
+	mov dh, 39 ; WIDTH, Draw sprite at X chunk
+	mov dl, 24 ; HEIGHT, Draw sprite at Y chunk
 	mov cx, SMILE2 ; Sprite we want to draw, this maps to an index in the include file 
 	call ShowSprite
 
 	mov dh, 0 ; WIDTH, Draw sprite at X chunk
+	mov dl, 15 ; HEIGHT, Draw sprite at Y chunk
+	mov cx, SMILE ; Sprite we want to draw, this maps to an index in the include file 
+	call ShowSprite
+
+	mov dh, 15 ; WIDTH, Draw sprite at X chunk
 	mov dl, 15 ; HEIGHT, Draw sprite at Y chunk
 	mov cx, SMILE ; Sprite we want to draw, this maps to an index in the include file 
 	call ShowSprite
