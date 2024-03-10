6723b5aab0fa09062e9b73467497494c8e08b2a5
jeanpierre <jeanpierre@derbes.net>
2024-03-09 21:54:43 -0500

    qol: sprite mapping

--- src/sprite/example.asm
+++ src/sprite/example.asm
@@ -6,34 +6,22 @@ include ../macros.inc
 include module.inc
 
 .data
-sprite db 00h, 00h, 0Eh, 0Eh, 0Eh, 0Eh, 00h, 00h
-		 db 00h, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 00h
-		 db 0Eh, 0Eh, 03h, 0Eh, 0Eh, 03h, 0Eh, 0Eh
-		 db 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh
-		 db 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh, 0Eh
-		 db 0Eh, 0Eh, 02h, 0Eh, 0Eh, 02h, 0Eh, 0Eh
-		 db 00h, 0Eh, 0Eh, 02h, 02h, 0Eh, 0Eh, 00h
-		 db 00h, 00h, 0Eh, 0Eh, 0Eh, 0Eh, 00h, 00h
  
 .code
 main PROC
 	setup
-	mov ax, @data
-	mov ds, ax
 
-	; Let's draw the sprite at chunk DH, DL
-	mov dh, 15 ; WIDTH, X chunk
-	mov dl, 15 ; HEIGHT, Y chunk
-	; Sprite location in memory
-
-   ; TODO: Rather than doing this, I want to have all sprites defined in module.inc, and
-   ;       we just pass a key in some register, and then the ShowSprite takes care of CH and Cl
-   ;       this will make it so user only needs to pass location, and the key of the sprite to draw
-	mov si, OFFSET sprite
-	; Dimensions of sprite
-	mov ch, 8 ; Width
-	mov cl, 8 ; Height
+	mov dh, 15 ; WIDTH, Draw sprite at X chunk
+	mov dl, 15 ; HEIGHT, Draw sprite at Y chunk
+	mov cx, SMILE ; Sprite we want to draw, this maps to an index in the include file 
 	call ShowSprite
+
+	mov dh, 0 ; WIDTH, Draw sprite at X chunk
+	mov dl, 15 ; HEIGHT, Draw sprite at Y chunk
+	mov cx, SMILE ; Sprite we want to draw, this maps to an index in the include file 
+	call ShowSprite
+
+		
 		
 	exit
 main ENDP
