1c9b52ebe8f57d9f5fcd923b20a4e19c6c10cd23
Arcey <44277279+Arcey-bot@users.noreply.github.com>
2024-03-12 19:20:56 -0400

    create cursor sprite

--- src/sprite/example.asm
+++ src/sprite/example.asm
@@ -12,6 +12,11 @@ include module.inc
 main PROC
 	setup
 
+	mov dh, 20 ; WIDTH, Draw sprite at X chunk
+	mov dl, 20 ; HEIGHT, Draw sprite at Y chunk
+	mov cx, CURSOR ; Sprite we want to draw, this maps to an index in the include file 
+	call ShowSprite
+
 	mov dh, 39 ; WIDTH, Draw sprite at X chunk
 	mov dl, 24 ; HEIGHT, Draw sprite at Y chunk
 	mov cx, SMILE2 ; Sprite we want to draw, this maps to an index in the include file 
