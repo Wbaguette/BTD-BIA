4c131018af96d30f2fc32e47e93285bfb938669e
Vincent <44277279+Arcey-bot@users.noreply.github.com>
2024-03-19 12:53:55 -0400

    movement kinda does stuff

--- src/game.asm
+++ src/game.asm
@@ -7,13 +7,23 @@ include const.inc
 include cursor.inc
 include stage.inc
 include macros.inc
+include bloon.inc
 
+.data 
+    red1 BLOON <RED_BLOON, 0>
 .code
 main PROC
     setup ;set video mode 
 
-    call InitStage
-    call DrawCursor
+    call InitStage ; onetime background initialization
+    call DrawCursor ; onetime cursor intitialization
+
+    gameloop:
+        xor bx, bx
+        mov bl, red1.pathIndex
+        lea dx, [PATH+bx]
+        mov cx, red1.level
+        call ShowSprite
     
     awaitkey: ; terminates program on key press
         mov ah, 10h
@@ -32,8 +42,26 @@ main PROC
         je placeDart
         cmp al, 'b'
         je placeBloon 
+        cmp al, 'c'
+        je continloon
         jmp awaitkey ; space was not pressed
     
+    continloon: 
+        xor bh, bh
+        mov bl, red1.pathIndex
+        lea dx, [PATH+bx]
+        mov cx, red1.level
+        ; call ShowSprite ; delete current bloon
+
+        add red1.pathIndex, 2 ; move bloon
+        ; mov ax, @data
+        ; mov ds, ax
+        ; mov bx, OFFSET red1
+
+        ; call Step ; move bloon to  next position
+        jmp gameloop
+        ; jmp awaitkey
+
     placeBloon:
         call GetPos
         mov cl, RED_BLOON
