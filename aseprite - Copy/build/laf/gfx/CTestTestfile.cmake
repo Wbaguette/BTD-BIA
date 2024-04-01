8f93f129fa97e82f67f0a1eeab4fb9327c30a37e
jeanpierre <jeanpierre@derbes.net>
2024-04-01 18:25:56 -0400

    remove aesprite

--- aseprite - Copy/build/laf/gfx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/aseprite/laf/gfx
-# Build directory: C:/aseprite/build/laf/gfx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- aseprite - Copy/build/third_party/cityhash/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/aseprite/third_party/cityhash
-# Build directory: C:/aseprite/build/third_party/cityhash
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
