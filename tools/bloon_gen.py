41b64537220b09566ca4752cb272371aace35132
Vincent <44277279+Arcey-bot@users.noreply.github.com>
2024-04-02 12:13:44 -0400

    tooling

--- tools/bloon_gen.py
+++ tools/bloon_gen.py
@@ -1,5 +1,16 @@
-def main():
-    ...
+import re
+import json
+import os
+
+RED_SPEED = 3
+BLUE_SPEED = 2
+GREEN_SPEED = 1 # and up
+
+def main(src):
+    res = "bloon_rounds.txt"
+    
+    data = json.loads(open(src))
 
 if __name__ == '__main__':
-    main()
+    src = "bloon_registry.json"
+    main(src)
