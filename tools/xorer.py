574bf6bd19f9af4a09bb964147d97d93fb1e448f
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2024-04-01 23:23:16 -0400

    update

--- tools/xorer.py
+++ tools/xorer.py
@@ -3,9 +3,9 @@ import glob
 import re
 
 # XOR each byte with background (02h), replace the byte with the result
-bg = int('2', 16)
+bg = int('8', 16)
 def main():   
-    for f in glob.glob('../sprites/monkey_bar/*.txt'):
+    for f in glob.glob('../sprites/bloon_pink.txt'):
         print(f'File {f}')
         with open(f, 'r+') as ff:
             # overwrite every string matching the regex with another string
