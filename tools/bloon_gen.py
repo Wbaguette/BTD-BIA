272438a48448d903a51e31e4e86a49426d4ea95c
Arcey <44277279+Arcey-bot@users.noreply.github.com>
2024-04-22 00:49:49 -0400

    rounds added and halved from source

--- tools/bloon_gen.py
+++ tools/bloon_gen.py
@@ -22,6 +22,7 @@ damage = {
     "WHITE": 7,
 }
 default_state = 1
+BLOONS_PER_LINE = 7
 
 def main(src):
     res = "bloon_rounds.txt"
@@ -43,25 +44,37 @@ def main(src):
             bloon, amt, frame = wave
             # print(f"Wave: {bloon} {amt} at frame {frame}")
             
-            for i in range(1, int(amt) + 1):              
+            for i in range(int(amt)):              
                 # subsequent bloons will be spawned after the last bloon has finished
                 timeout = frame + (i * speed[bloon]) 
                 bloons.append(f'<BLOON_{bloon}, 0, {speed[bloon]}, {timeout}, {damage[bloon]}, {default_state}>')
                         
         # save bloons as new line in res, create if not exists
+        # rounds are 0-indexed in code
         with open(res, "a") as f:
-            f.write(f'r{round}_queue_length dw {len(bloons)}\n')
-            f.write(f'r{round}_queue BLOON ')
+            f.write(f'r{int(round)-1}_queue_length dw {len(bloons)}\n')
+            f.write(f'r{int(round)-1}_queue BLOON ')
             
             # write every bloon, 8 per line
             for i, bloon in enumerate(bloons):
                 f.write(bloon)
-                if i % 8 == 7:
-                    f.write("\n\t")
+                if i % BLOONS_PER_LINE == BLOONS_PER_LINE - 1:
+                    f.write("\n\t BLOON ")
                 elif i != len(bloons) - 1:
                     f.write(", ")
                     
             f.write("\n\n")
+    
+    with open(res, "a") as f:
+        f.write("queue_registry dw ")
+        
+        for i in range(len(data.keys())):
+            f.write(f"OFFSET r{i}_queue")
+            
+            if i % BLOONS_PER_LINE == BLOONS_PER_LINE - 1:
+                f.write("\n\t dw ")
+            elif i != len(data.keys()) - 1:
+                f.write(", ")
 
 if __name__ == '__main__':
     src = "bloon_registry.json"
